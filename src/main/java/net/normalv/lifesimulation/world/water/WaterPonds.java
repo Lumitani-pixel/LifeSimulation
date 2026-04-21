d4e62d42182abeab14f7ad894e6acb330c29d6f1
NormalV <moritz@fackler.cloud>
2026-04-21 16:51:42 +0200

    add: Graphical representation for WaterPonds

--- src/main/java/net/normalv/lifesimulation/world/water/WaterPonds.java
+++ src/main/java/net/normalv/lifesimulation/world/water/WaterPonds.java
@@ -1,25 +1,31 @@
 package net.normalv.lifesimulation.world.water;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 import java.util.Random;
 
 public class WaterPonds {
     private int x;
     private int y;
     private int waterAmount;
+    private Circle circle;
 
     public WaterPonds(int x, int y, int waterAmount) {
         this.x = x;
         this.y = y;
         this.waterAmount = waterAmount;
+        this.circle = new Circle(x, y, waterAmount, Color.BLUE);
     }
 
     public static WaterPonds[] createWaterPonds(int sizex, int sizey, int amount) {
-       Random random = new Random();
-      WaterPonds[] waterPond = new WaterPonds[amount];
-      for (int i = 0; i < amount; i++) {
-         waterPond[i] = new WaterPonds(random.nextInt(sizex, sizey), random.nextInt(sizex, sizey), random.nextInt(1, 100));
-      }
-       return waterPond;
+        Random random = new Random();
+        WaterPonds[] waterPond = new WaterPonds[amount];
+
+        for (int i = 0; i < amount; i++) {
+            waterPond[i] = new WaterPonds(random.nextInt(sizex, sizey), random.nextInt(sizex, sizey), random.nextInt(1, 100));
+        }
+        return waterPond;
     }
 
     public int getX() {
@@ -32,5 +38,9 @@ public class WaterPonds {
 
     public int getWaterAmount() {
         return waterAmount;
+    }
+
+    public Circle getCircle() {
+        return this.circle;
     }
 }
