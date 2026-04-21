829adc230e4b46e4d08ac92dc4fbde16edd506d4
NormalV <moritz@fackler.cloud>
2026-04-21 16:53:32 +0200

    add: Graphical representation for Apples

--- src/main/java/net/normalv/lifesimulation/world/food/Apple.java
+++ src/main/java/net/normalv/lifesimulation/world/food/Apple.java
@@ -1,8 +1,17 @@
 package net.normalv.lifesimulation.world.food;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 public class Apple extends FoodItem{
+    private Circle circle;
 
     public Apple(int x, int y) {
         super(x, y, 20, 40);
+        this.circle = new Circle(x, y, 5, Color.RED);
+    }
+
+    public Circle getCircle() {
+        return this.circle;
     }
 }
